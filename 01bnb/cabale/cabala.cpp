cd9be343a051d05a753662fb0a6f67c51e97bac0
EndroidMc <raphael.siller123@gmail.com>
2022-05-19 22:15:25 +0200

    added new (still not finished) version of cabala

--- 01bnb/cabale/cabala.cpp
+++ 01bnb/cabale/cabala.cpp
@@ -1,11 +1,50 @@
 #include <stdio.h>
 #include <assert.h>
+#include <vector>
+#include <string>
+#include <math.h>
 
-long long occulta(int N, int M) {
+std::vector<int> possibleNumbers;
+
+bool isPossibleNumber(int maybePossibleNumber);
+int cabala(int M);
+
+int occulta(int N, int M) {
+    for (int maybePossibleNumber = pow(10, N)-1; maybePossibleNumber > 0; maybePossibleNumber-=3) {
+        if(isPossibleNumber(maybePossibleNumber)) {
+            possibleNumbers.push_back(maybePossibleNumber);
+        }
+    }
     
-    return 42;
+    return cabala(M);
 }
 
+bool isPossibleNumber(int maybePossibleNumber) {
+    bool ret = true;
+    std::string number = std::to_string(maybePossibleNumber);
+    for (int i = 0; i < number.length(); i++) {
+        if(number[i] != '3' && number[i] != '6' && number[i] != '9')
+            ret = false;
+    }
+    if (ret) {
+        for (int i = 0; i < number.length()-1; i++) {
+            if (number[i] == number[i+1]) {
+                ret = false;
+                i = number.length();
+            }
+        }
+    }
+    return ret;
+}
+
+int cabala(int M) {
+    int ret = possibleNumbers[0] % M;
+    for (int i = 1; i < possibleNumbers.size(); i++) {
+        if (possibleNumbers[i] % M > ret)
+            ret = possibleNumbers[i] % M;
+    }
+    return ret;
+}
 
 int main() {
     FILE *fr, *fw;
